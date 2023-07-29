361ef6a74fc633cf6f1ab1e81ab82e64a4b098a3
Nen Patel <nenspatel2001@gmail.com>
2023-07-29 14:09:30 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
 
   const userHomePage = async () => {
     try {
-        const res = await fetch("/getData",{
+        const res = await fetch("https://mernserver-wsyc.onrender.com/getData",{
             method:"GET",
             headers:{
                 "Content-Type" : "application/json"
